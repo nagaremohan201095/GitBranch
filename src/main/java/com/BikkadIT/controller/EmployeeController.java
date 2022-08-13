40fdd7f01bc666254646cbfdee94f5537a647b25
nagaremohan201095 <nagaremohan201095@gmail.com>
2022-08-13 17:21:43 +0530

    AR-1 : Employee Controller Added

---
+++ src/main/java/com/BikkadIT/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	public void addEmpoyee() {
+		
+		
+	}
+
+}
